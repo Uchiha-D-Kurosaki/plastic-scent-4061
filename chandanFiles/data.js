3edfdf5a52ab5677a55bdbaae43d448406c97419
Chandan Sharma <chandanshrma984@gmail.com>
2022-08-24 21:34:56 +0530

    chandan day2 files

---
+++ chandanFiles/data.js
@@ -0,0 +1,42 @@
+data=[
+    {
+        img1:"https://cdn04.nnnow.com/web-images/medium/styles/E8MATF2IDYU/1605084283131/1.jpg",
+        img2:"https://cdn07.nnnow.com/web-images/medium/styles/E8MATF2IDYU/1605084283128/2.jpg",
+        titile:"Elizabeth Arden",
+        name:"Green Tea Scent Spray",
+        price:"2,675",
+    },
+    {
+        img1:"https://cdn14.nnnow.com/web-images/medium/styles/GX1INCBAMS6/1487245292188/1.jpg",
+        img2:"https://cdn14.nnnow.com/web-images/medium/styles/GX1INCBAMS6/1487245292188/1.jpg",
+        titile:"DAVIDOFF",
+        name:"Cool Water Sea Rose Eau De Toilette",
+        price:"4600",
+    },
+    {
+        img1:"https://cdn06.nnnow.com/web-images/medium/styles/MI7U8R8C848/1580463748784/1.jpg",
+        img2:"https://cdn15.nnnow.com/web-images/medium/styles/MI7U8R8C848/1580463748782/2.jpg",
+        titile:"BURBERRY",
+        name:"Her Intense Eau De Parfum",
+        price:"8250",
+    },
+  
+
+
+];
+
+
+
+
+// let container=document.getElementById("product")
+// let img=document.createElement("img");
+// img.src="https://cdn07.nnnow.com/web-images/medium/styles/E8MATF2IDYU/1605084283128/2.jpg"
+// img.onmouseover=function hchange(){
+//     img.src="https://cdn12.nnnow.com/web-images/medium/styles/W78W9RM2233/1637735704512/1.jpg"
+// }
+// img.onmouseleave=function leave(){
+//     img.src="https://cdn07.nnnow.com/web-images/medium/styles/E8MATF2IDYU/1605084283128/2.jpg"
+// }
+// container.append(img)
+// // "https://cdn12.nnnow.com/web-images/medium/styles/W78W9RM2233/1637735704512/1.jpg"
+
